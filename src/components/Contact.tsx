6a9fe79ec030685fa1626e0e35d83e2c3a704299
Yash Chaudhari <yashchaudhari9599@gmail.com>
2025-06-26 23:30:58 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -54,7 +54,7 @@ const Contact: React.FC = () => {
             type="submit"
             className="w-full bg-gradient-to-r from-purple-500 to-indigo-500 hover:from-purple-600 hover:to-indigo-600 text-white font-semibold py-3 rounded-xl transition"
           >
-            Send Message
+            Contact me
           </button>
         </motion.form>
       </div>
